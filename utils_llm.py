f861cc75fb870f71d7743ac584ce2e164a7b39bb
Money <manhtien310701@gmail.com>
2024-08-27 16:45:59 +0800

    add evaluate-model-by-dataset-sql

--- utils_llm.py
+++ utils_llm.py
@@ -69,29 +69,29 @@ def get_default_finetune_args():
   
 def get_schema():
   return """\
-  0|Team|TEXT eg. "Toronto Raptors"
-  1|NAME|TEXT eg. "Otto Porter Jr."
-  2|Jersey|TEXT eg. "0" and when null has a value "NA"
-  3|POS|TEXT eg. "PF"
-  4|AGE|INT eg. "22" in years
-  5|HT|TEXT eg. `6' 7"` or `6' 10"`
-  6|WT|TEXT eg. "232 lbs" 
-  7|COLLEGE|TEXT eg. "Michigan" and when null has a value "--"
-  8|SALARY|TEXT eg. "$9,945,830" and when null has a value "--"
+  0|team|TEXT eg. "Toronto Raptors"
+  1|name|TEXT eg. "Otto Porter Jr."
+  2|jersey|TEXT eg. "0" and when null has a value "NA"
+  3|pos|TEXT eg. "PF"
+  4|age|INT eg. "22" in years
+  5|ht|TEXT eg. `6' 7"` or `6' 10"`
+  6|wt|TEXT eg. "232 lbs" 
+  7|college|TEXT eg. "Michigan" and when null has a value "--"
+  8|salary|TEXT eg. "$9,945,830" and when null has a value "--"
   """
 
 #slightly modified schema to match filming
 def get_schema_s():
   return """\
-  0|Team|TEXT eg. "Toronto Raptors"
-  1|NAME|TEXT eg. "Otto Porter Jr."
-  2|Jersey|TEXT eg. "0" and when null has a value "NA"
-  3|POS|TEXT eg. "PF"
-  4|AGE|INT eg. "22" in years
-  5|HT|TEXT eg. `6' 7"` or `6' 10"` castable to int
-  6|WT|TEXT eg. "232 lbs" 
-  7|COLLEGE|TEXT eg. "Michigan" and when null has a value "--"
-  8|SALARY|TEXT eg. "$9,945,830" and when null has a value "--"
+  0|team|TEXT eg. "Toronto Raptors"
+  1|name|TEXT eg. "Otto Porter Jr."
+  2|jersey|TEXT eg. "0" and when null has a value "NA"
+  3|pos|TEXT eg. "PF"
+  4|age|INT eg. "22" in years
+  5|ht|TEXT eg. `6' 7"` or `6' 10"` castable to int
+  6|wt|TEXT eg. "232 lbs" 
+  7|college|TEXT eg. "Michigan" and when null has a value "--"
+  8|salary|TEXT eg. "$9,945,830" and when null has a value "--"
   """
   
 def get_rubric():
